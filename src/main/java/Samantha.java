fe650e99e6f0801e67b28e063ac81d0f3e7b33bd
Xzh119 <>
2025-01-31 00:27:00 +0800

    Add Echo

--- src/main/java/Samantha.java
+++ src/main/java/Samantha.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Samantha {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Samantha");
-        System.out.println(" What can I do for you?");
+        System.out.println("    Hello! I'm Samantha");
+        System.out.println("    What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
+
+        Scanner scanner = new Scanner(System.in);
+        String userCommand = scanner.nextLine();
+        while (!userCommand.equals("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println("    " + userCommand);
+            System.out.println("____________________________________________________________");
+            userCommand = scanner.nextLine();
+        }
+        scanner.close();
+        System.out.println("____________________________________________________________");
+        System.out.println("    Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
 }
