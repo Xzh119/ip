211a6a30497eb9b0e6417f81b127f08a5599bf14
Xzh119 <>
2025-01-31 01:02:51 +0800

    Add list and add ability

--- src/main/java/Samantha.java
+++ src/main/java/Samantha.java
@@ -6,13 +6,34 @@ public class Samantha {
         System.out.println("    Hello! I'm Samantha");
         System.out.println("    What can I do for you?");
         System.out.println("____________________________________________________________");
-
+        String[] tasks = new String[100];
+        int taskSum = 0;
         Scanner scanner = new Scanner(System.in);
         String userCommand = scanner.nextLine();
         while (!userCommand.equals("bye")) {
-            System.out.println("____________________________________________________________");
+            /*System.out.println("____________________________________________________________");
             System.out.println("    " + userCommand);
-            System.out.println("____________________________________________________________");
+            System.out.println("____________________________________________________________");*/
+            if (!userCommand.equals("list")) {
+                if (taskSum > 99) {
+                    System.out.println("    List is full.");
+                }else {
+                    tasks[taskSum] = userCommand;
+                    taskSum++;
+                    System.out.println("____________________________________________________________");
+                    System.out.println("    added: " + userCommand);
+                    System.out.println("____________________________________________________________");
+                }
+            } else {
+                System.out.println("____________________________________________________________");
+                if (taskSum == 0) {
+                    System.out.println("    List is empty.");
+                }
+                for (int i = 1; i <= taskSum; i++) {
+                    System.out.println("    " + i + ". " + tasks[i-1]);
+                }
+                System.out.println("____________________________________________________________");
+            }
             userCommand = scanner.nextLine();
         }
         scanner.close();
